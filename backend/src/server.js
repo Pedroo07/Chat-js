1e46689883b4858b898e9ad760d3affbfe558869
Pedroo <pedrohenriquemoalive@gmail.com>
2024-02-15 23:17:24 -0300

    Exibindo mensagem do usuario na tela

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,8 @@ const wss = new WebSocketServer({port: process.env.PORT})
 wss.on("connection", (ws) => {
     ws.on("error", console.error)
 
+    ws.send('Msg text')
+
     ws.on("message", (data) => {
         wss.clients.forEach((client) => client.send(data.toString()))
     })
--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,11 @@
 const login = document.querySelector('.login')
 const loginForm = document.querySelector('.login_form')
-const loginInput= document.querySelector('.login_input')
+const loginInput = document.querySelector('.login_input')
 
 const chat = document.querySelector('.chat')
 const chatForm = document.querySelector('.chat_form')
-const chatInput= document.querySelector('.chat_input')
+const chatInput = document.querySelector('.chat_input')
+const chatMsg = document.querySelector('.chat_messages')
 
 const colors = [
     "aqua",
@@ -18,26 +19,57 @@ const colors = [
 const user = {
     id: "",
     name: "",
-    color:  ""
+    color: ""
 }
+
+
 
 let websocket
 
+const createdMessageSelf = (content) => {
+const div = document.createElement('div')
+div.classList.add('send_message')
+div.innerHTML = content
+return div
+} 
+
 const getRandomColor = () => {
-    const randomIndex =Math.floor(Math.random() * colors.length
-)
-return colors[randomIndex]
+    const randomIndex = Math.floor(Math.random() * colors.length)
+    return colors[randomIndex]
+}
+const processMessage = ({ data }) => {
+    const { userId, userColor, userName, content } = (JSON.parse(data))
+    const element = createdMessageSelf(content)
+    chatMsg.appendChild(element)
 }
 
 const handleSubmit = (event) => {
-event.preventDefault()
-user.id = crypto.randomUUID()
-user.name = loginInput.value
-user.color = getRandomColor()
-login.style.display = 'none'
-chat.style.display = "flex"
-websocket = new WebSocket('ws://localhost:8080')
-console.log(user)
+    event.preventDefault()
+
+    user.id = crypto.randomUUID()
+    user.name = loginInput.value
+    user.color = getRandomColor()
+
+    login.style.display = 'none'
+    chat.style.display = "flex"
+
+    websocket = new WebSocket('ws://localhost:8080')
+    websocket.onmessage = processMessage
+
+}
+const handleSendMessage = (event) => {
+    event.preventDefault()
+
+    const message = {
+        userId: user.id,
+        userName: user.name,
+        userColor: user.color,
+        content: chatInput.value
+    }
+
+    websocket.send(JSON.stringify(message))
+    chatInput.value = ''
 }
 
 loginForm.addEventListener('submit', handleSubmit)
+chatForm.addEventListener('submit', handleSendMessage)
